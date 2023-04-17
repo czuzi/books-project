9097a16d4fd5e4ddf6d02f9a58899f8c4a17345b
Istvan <palocsayi@gmail.com>
2023-04-17 13:38:13 +0200

    initial commit

---
+++ books-project/src/main/java/training360/booksproject/model/Book.java
@@ -0,0 +1,5 @@
+package training360.booksproject.model;
+
+
+public class Book {
+}
---
+++ books-project/src/test/java/training360/booksproject/BooksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package training360.booksproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
